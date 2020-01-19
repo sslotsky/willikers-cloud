f8c26eff71138a41c3243aa01fddc57a9603de2d
Sam Slotsky <sam@manifold.co>
2020-01-19 08:36:50 -0600

    Setup DB and webhook route

---
+++ src/entity/Plan.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, Index } from "typeorm";
+
+@Entity()
+export class Plan {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index({ unique: true })
+  @Column()
+  name: string;
+
+  @Column("int")
+  amount: number;
+
+  @Column()
+  currency: string;
+
+  @Column()
+  interval: "string";
+}
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,33 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
-import {User} from "./entity/User";
+import { config } from "dotenv";
 
-createConnection().then(async connection => {
+config();
 
-    // create express app
+createConnection({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: "gee-willikers",
+  entities: [__dirname + "/entity/*{.js,.ts}"],
+  synchronize: true
+})
+  .then(async connection => {
     const app = express();
     app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
 
-    // register express routes from defined application routes
-    Routes.forEach(route => {
-        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-            const result = (new (route.controller as any))[route.action](req, res, next);
-            if (result instanceof Promise) {
-                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-            } else if (result !== null && result !== undefined) {
-                res.json(result);
-            }
-        });
+    app.post("/webhook", (req, res) => {
+      console.log(req.headers["content-type"], req.body);
+      res.json(req.body);
     });
 
-    // setup express app here
-    // ...
-
-    // start express server
     app.listen(3000);
 
-    // insert new users for test
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Timber",
-        lastName: "Saw",
-        age: 27
-    }));
-    await connection.manager.save(connection.manager.create(User, {
-        firstName: "Phantom",
-        lastName: "Assassin",
-        age: 24
-    }));
-
-    console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results");
-
-}).catch(error => console.log(error));
+    console.log("Express server has started on port 3000.");
+  })
+  .catch(error => console.log(error));
